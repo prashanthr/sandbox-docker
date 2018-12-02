3f254c61e23d82749138be245879ff7a970a4fea
Prash <prashanth@slice.com>
2018-12-02 12:59:45 -0800

    feat(docker): Add docker file and node app

--- node/bin/start.sh
+++
@@ -1,1 +0,0 @@
-./node_modules/.bin/babel-node ./src/index.js
--- node/src/index.js
+++ node/src/index.js
@@ -1,6 +1,19 @@
-const app = () => {
-  console.log('Hello World!')
+import express from 'express'
+import http from 'http'
+
+const serverPort = process.env.PORT
+const app = express()
+app.use('/health', (req, res, next) => {
+  console.log('Health check')
+  res.json({ status: 'healthy'})
+})
+const server = async () => {
+  console.log('Hello World! This is a sample node application')
   console.log('Environment Vars: ', process.env)
+  let httpServer = http.Server(app)
+  httpServer.listen(serverPort, () => {
+    console.log(`Server running on ${serverPort}`)
+  })
 }
 
-app()
+server()
